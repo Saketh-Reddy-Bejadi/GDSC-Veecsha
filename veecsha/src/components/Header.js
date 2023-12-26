a9f0ac5bcfc0b0d98a2b4489646ef99b3c75250a
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2023-12-27 00:15:25 +0530

    working on navbar

--- veecsha/src/components/Header.js
+++ veecsha/src/components/Header.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css";
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <nav>
+        <img src="https://cdn-icons-png.flaticon.com/128/5968/5968866.png" height="60px" alt="" />
+        <ul>
+          <li className='optdrop' >Roadmaps</li>
+          <li className='optdrop' >Cheatsheets</li>
+          <li className='optdrop' >Installation</li>
+
+        </ul>
+      </nav>
+    </>
   )
 }
