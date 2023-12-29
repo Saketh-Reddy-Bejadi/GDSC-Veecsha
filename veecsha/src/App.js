e0aaf3c6f7c43632f97d75306cea78dc7f687fc7
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2023-12-29 23:32:24 +0530

    simply modified everything

--- veecsha/src/App.js
+++ veecsha/src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
-
+import './components/Page1.css';
+import './components/Header.css';
 import React from 'react'
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import { Page1 } from './components/Page1';
+import veecsha from "./assets/Veecsha.png"
+
 
 export const App = () => {
   return (
     <>
-      <Header/>
-      <Page1/>
-      <Footer/>
+      <div className="main">
+        <div className='navbar'>
+          <img src={veecsha} height="30px" alt="" />
+          <ul >
+            <a href="Roadmaps"><li className='optdrop' >Web Development</li></a>
+            <a href=""><li className='optdrop' >Data Structures & Algorithms</li></a>
+          </ul>
+        </div>
+        <div className='Page1'>
+          <p className='heading'>Unleash Your Power With</p>
+          <p className='highlighted'>Veecsha</p>
+          <p className='subheading'>Prefer progess over perfection</p>
+        </div>
+      </div>
     </>
   )
 }
