c178156580b6218479f12ac46f7eb239aabf67e9
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2023-12-30 23:04:57 +0530

    tried with menu bar

--- veecsha/src/App.js
+++ veecsha/src/App.js
@@ -2,21 +2,30 @@ import './App.css';
 import './components/Page1.css';
 import './components/Header.css';
 import React from 'react';
-import menu from './assets/menu.png'
 import veecsha from "./assets/Veecsha.png";
 
 
 export const App = () => {
+
+  const menu = document.getElementById("menu")
+  menu.addEventListener("click", () => { 
+    menu.classList.toggle("active");
+  });
+
   return (
     <>
       <div className="main">
         <div className='navbar'>
           <img src={veecsha} height="30px" alt="" />
-          <ul >
+          <ul>
             <a href="Roadmaps"><li className='optdrop' >Web Development</li></a>
             <a href=""><li className='optdrop' >Data Structures & Algorithms</li></a>
           </ul>
-          <img className='menu' src={menu} alt="" />
+          <div id="menu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
         <div className='Page1'>
           <p className='heading'>Unleash <span className='you' >You</span>r Power With</p>
