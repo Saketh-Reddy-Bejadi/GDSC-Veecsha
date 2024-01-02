0b47a9cd0b477395d79ec5677705d9956642ce51
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2024-01-02 22:13:35 +0530

    done with menu animation

--- veecsha/src/App.js
+++ veecsha/src/App.js
@@ -1,30 +1,26 @@
 import './App.css';
 import './components/Page1.css';
 import './components/Header.css';
+import Menu from './components/Menu';
 import React from 'react';
 import veecsha from "./assets/Veecsha.png";
 
 
 export const App = () => {
 
-  const menu = document.getElementById("menu")
-  menu.addEventListener("click", () => { 
-    menu.classList.toggle("active");
-  });
+
 
   return (
     <>
       <div className="main">
         <div className='navbar'>
           <img src={veecsha} height="30px" alt="" />
+          <div className='NavPart2' >
           <ul>
             <a href="Roadmaps"><li className='optdrop' >Web Development</li></a>
             <a href=""><li className='optdrop' >Data Structures & Algorithms</li></a>
           </ul>
-          <div id="menu">
-            <span></span>
-            <span></span>
-            <span></span>
+          <Menu/>
           </div>
         </div>
         <div className='Page1'>
