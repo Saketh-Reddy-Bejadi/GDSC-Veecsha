81beb456d3e97e42430fe1874684281a1d1171ad
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2024-01-13 22:01:09 +0530

    added logo

--- veecsha/src/components/Header.js
+++ veecsha/src/components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import Menu from './Menu'
 import { SideBar } from './SideBar'
-import veecsha from "../assets/Veecsha.png";
+import veecsha from "../assets/20240113_213310_0000 copy.svg";
 
 
 export const Header = ({MenuOpen,viewBox}) => {
   return (
     <div>
         <div className='navbar'>
-          <img src={veecsha} height="30px" alt="" />
+          <img className='brnd' src={veecsha} alt="" />
           <div className='NavPart2' >
           <ul>
-            <a href="Web-development"><li className='optdrop' >Web Development</li></a>
-            <a href="DSA"><li className='optdrop'>Data Structures & Algorithms</li></a>
+            <a href="Web-development"><li className='navBtn webBtn' >Web Development</li></a>
+            <a href="DSA"><li className='navBtn dsaBtn'>Data Structures & Algorithms</li></a>
           </ul>
           <Menu MenuOpen={MenuOpen}/>
           <SideBar MenuOpen={MenuOpen} viewBox={viewBox}/>
