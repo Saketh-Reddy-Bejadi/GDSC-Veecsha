f469f8b992baf4b98f74cc4347dffc98f5674984
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2024-01-14 23:33:30 +0530

    changed btn styling and Veecsha brnd

--- veecsha/src/components/Page1.js
+++ veecsha/src/components/Page1.js
@@ -5,7 +5,7 @@ export const Page1 = () => {
   return (
     <div className='Page1'>
         <div className='Con1'>
-          <p className='Head1' >Introducing Veecsha</p>
+          <p className='Head1' >Introducing <span className='brnd' >Veecsha</span></p>
           <p className="Body1">
           Welcome to Veecsha - Where Tech Learning Feels Like a Breeze! 🎓✨<br/>
           In a world buzzing with tech complexities, Veecsha is your shortcut to expertise without the overload. Bid farewell to the confusion and embrace a straightforward journey to mastering the essential tech know-how.
