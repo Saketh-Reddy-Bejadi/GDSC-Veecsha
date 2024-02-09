dd9e60d622b161e24fe5378cbfb2d4ff38ba8d64
Saketh Reddy Bejadi <144515308+Saketh-Reddy-Bejadi@users.noreply.github.com>
2024-02-09 08:49:24 +0530

    Update Services.js

--- veecsha/src/components/Services.js
+++ veecsha/src/components/Services.js
@@ -9,7 +9,7 @@ export const Services = () => {
       behavior: 'smooth',
     };
     const mobileScrollPosition = 660;
-    const desktopScrollPosition = 660;
+    const desktopScrollPosition = 760;
     const isMobile = window.matchMedia('(max-width: 768px)').matches;
     const scrollPosition = isMobile ? mobileScrollPosition : desktopScrollPosition;
     window.scrollTo({
